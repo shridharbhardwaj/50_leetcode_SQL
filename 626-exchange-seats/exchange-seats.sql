87ced84cad738960a7f16d8ae15968ed09783882
Shridhar Bhardwaj <57531390+shridharbhardwaj@users.noreply.github.com>
2023-12-24 13:21:37 -0800

    Time: 642 ms (60.44%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,4 +1,8 @@
 # Write your MySQL query statement below
-select if(id < (select MAX(id) from seat), if(id % 2 = 0, id-1, id+1), if(id % 2 = 0, id-1, id)) AS id, student 
-from seat
-order by id;
+# select if(id < (select MAX(id) from seat), if(id % 2 = 0, id-1, id+1), if(id % 2 = 0, id-1, id)) AS id, student 
+# from seat
+# order by id;
+
+# Using CASE condition:
+select id, coalesce(case when id%2=0 then lag(student) over() else lead(student) over() end, student) as student
+from seat;
