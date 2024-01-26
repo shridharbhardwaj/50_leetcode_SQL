b5333c1199a7435be3273867048a64ec03789bd1
Shridhar Bhardwaj <57531390+shridharbhardwaj@users.noreply.github.com>
2024-01-25 19:33:39 -0800

    Time: 477 ms (35.09%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -4,7 +4,20 @@
 -- where a.activity_type = 'start' and b.activity_type = 'end'
 -- group by a.machine_id;
 
-select machine_id, 
-round(avg(case when activity_type='end' then timestamp end)-avg(case when activity_type='start' then timestamp end),3) as processing_time
-from Activity
+-- select machine_id, 
+-- round(avg(case when activity_type='end' then timestamp end)-avg(case when activity_type='start' then timestamp end),3) as processing_time
+-- from Activity
+-- group by machine_id;
+
+select machine_id, round(avg (end_timestamp - start_timestamp),3) as processing_time
+from
+(
+        select
+            machine_id,
+            process_id,
+            SUM(if(activity_type ='start', timestamp,null)) AS start_timestamp,
+            SUM(if(activity_type ='end', timestamp,null)) AS end_timestamp
+        from Activity
+        group by machine_id, process_id
+) as t
 group by machine_id
