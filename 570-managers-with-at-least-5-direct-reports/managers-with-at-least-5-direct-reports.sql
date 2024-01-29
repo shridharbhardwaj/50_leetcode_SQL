a0820f0b2a474447e9b47288e31b41650b9cd1c8
Shridhar Bhardwaj <57531390+shridharbhardwaj@users.noreply.github.com>
2024-01-28 17:08:49 -0800

    Time: 855 ms (18.37%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -5,11 +5,19 @@
 -- order by b.managerId;
 
 -- *********************************************************
-SELECT E1.name
-FROM Employee E1
-JOIN (
-    SELECT managerId, COUNT(*) AS directReports
-    FROM Employee
-    GROUP BY managerId
-    HAVING COUNT(*) >= 5
-) E2 ON E1.id = E2.managerId;
+-- SELECT E1.name
+-- FROM Employee E1
+-- JOIN (
+--     SELECT managerId, COUNT(*) AS directReports
+--     FROM Employee
+--     GROUP BY managerId
+--     HAVING COUNT(*) >= 5
+-- ) E2 ON E1.id = E2.managerId;
+-- *********************************************************
+
+select e1.name from employee e1
+where e1.id in (
+                  select e2.managerId 
+                   from employee e2
+                    group by e2.managerId
+                    having count(e2.managerId) >= 5);
