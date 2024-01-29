64b19d5a2e79eeebf183f3357f4bf4bba1e2eb7c
Shridhar Bhardwaj <57531390+shridharbhardwaj@users.noreply.github.com>
2024-01-28 16:57:30 -0800

    Time: 537 ms (89.29%) | Memory: 0B (100.00%) - LeetSync

---
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+-- select a.name 
+-- from employee a JOIN employee b 
+-- ON a.id = b.managerId and a.id <> b.id
+-- having count(b.managerId) >= 5
+-- order by a.name;
+
+select a.name as name
+from employee a join employee b on a.id = b.managerId
+group by b.managerId
+having count(b.managerId) >= 5
+order by a.name;
