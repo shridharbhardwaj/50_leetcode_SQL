f65827ee56ae9c9da469c4b66cf0415b91b2faa6
Shridhar Bhardwaj <57531390+shridharbhardwaj@users.noreply.github.com>
2024-01-28 17:09:06 -0800

    Time: 569 ms (76.51%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,8 +1,8 @@
--- select a.name 
--- from employee a inner join employee b on a.id = b.managerId
--- group by b.managerId
--- having count(b.managerId) >= 5
--- order by b.managerId;
+select a.name 
+from employee a inner join employee b on a.id = b.managerId
+group by b.managerId
+having count(b.managerId) >= 5
+order by b.managerId;
 
 -- *********************************************************
 -- SELECT E1.name
@@ -15,9 +15,9 @@
 -- ) E2 ON E1.id = E2.managerId;
 -- *********************************************************
 
-select e1.name from employee e1
-where e1.id in (
-                  select e2.managerId 
-                   from employee e2
-                    group by e2.managerId
-                    having count(e2.managerId) >= 5);
+-- select e1.name from employee e1
+-- where e1.id in (
+--                   select e2.managerId 
+--                    from employee e2
+--                     group by e2.managerId
+--                     having count(e2.managerId) >= 5);
