9a1bce13191d94f32bccb1c8ed4f9cb23aceac1d
Shridhar Bhardwaj <57531390+shridharbhardwaj@users.noreply.github.com>
2024-02-01 20:04:07 -0800

    Time: 707 ms (13.80%) | Memory: 0B (100.00%) - LeetSync

--- 610-triangle-judgement/triangle-judgement.sql
+++ 610-triangle-judgement/triangle-judgement.sql
@@ -6,5 +6,6 @@
 -- from Triangle t;
 
 -- ********************************************************************************
-select x,y,z, (select if((x+y>z and y+z>x and x+z>y),'Yes','No') end) as triangle
-from Triangle t;
+select x,y,z, (select if((x+y>z and y+z>x and x+z>y),'Yes','No') end) as triangle from Triangle t;
+
+-- ********************************************************************************
