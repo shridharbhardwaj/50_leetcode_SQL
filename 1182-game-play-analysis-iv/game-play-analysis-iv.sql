632430e9c4e66975e268a482466a2f6737e0039f
Shridhar Bhardwaj <57531390+shridharbhardwaj@users.noreply.github.com>
2024-02-03 23:50:14 -0500

    Time: 1336 ms (20.19%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,4 +1,14 @@
 # Write your MySQL query statement below
+
+select 
+round(count(distinct player_id)/(select count(distinct player_id) from activity),2) as fraction
+from Activity 
+where (player_id, date_sub(event_date, interval 1 day)) in 
+    (select player_id,min(event_date) as first_login_date
+    from activity group by player_id
+    );
+
+-- ********************************************************************************************************
 -- 14/15 test cases passed
 -- select 
 -- round(sum(case when s.first_login_date + 1 = a.event_date then 1 else 0 end)/count(distinct s.player_id),2) as fraction
@@ -8,11 +18,4 @@
 -- join activity a
 -- on s.player_id=a.player_id;
 
--- ********************************************************************************************************
-select 
-round(count(distinct player_id)/(select count(distinct player_id) from activity),2) as fraction
-from Activity 
-where (player_id, date_sub(event_date, interval 1 day)) in 
-    (select player_id,min(event_date) as first_login_date
-    from activity group by player_id
-    )
+
