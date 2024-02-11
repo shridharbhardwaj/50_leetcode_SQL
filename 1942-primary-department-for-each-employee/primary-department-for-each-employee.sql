dd66f22bbf71c57a27311089cc69e98de7a91fa9
Shridhar Bhardwaj <57531390+shridharbhardwaj@users.noreply.github.com>
2024-02-11 14:30:08 -0500

    Time: 1347 ms (15.78%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -0,0 +1,21 @@
+# Write your MySQL query statement below
+select distinct employee_id, department_id
+from employee
+where employee_id in (select employee_id 
+                      from employee 
+                      group by employee_id
+                      having count(primary_flag) = 1 ) or
+primary_flag = 'Y';
+
+
+-- *********************************************************************************************
+-- select employee_id,
+-- case 
+--      when (count(primary_flag) = 1 and max(primary_flag) = 'N') then max(department_id)
+--      when (count( distinct primary_flag) > 1 and primary_flag= 'Y') then department_id
+--      else null
+-- end as department_id
+-- from Employee
+-- group by employee_id;
+
+-- *********************************************************************************************
