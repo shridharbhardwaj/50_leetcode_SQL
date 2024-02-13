e4fe39a12783206c625e1bb134534eb087e7c92e
Shridhar Bhardwaj <57531390+shridharbhardwaj@users.noreply.github.com>
2024-02-12 19:36:50 -0500

    Time: 925 ms (98.82%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -3,6 +3,6 @@ select a.employee_id, a.name, count(b.reports_to) as reports_count, round(avg(b.
 from employees a join employees b on a.employee_id = b.reports_to
 group by a.employee_id
 order by a.employee_id;
--- having count(b.reports_to) > 1;
+
 
 
