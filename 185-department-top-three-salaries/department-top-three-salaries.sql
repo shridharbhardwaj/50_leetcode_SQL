cd3cef5b6ed2cf0c7848e67b70fb928185ed8865
Shridhar Bhardwaj <57531390+shridharbhardwaj@users.noreply.github.com>
2024-02-13 02:20:29 -0500

    Time: 1491 ms (86.82%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,16 +1,25 @@
 # Write your MySQL query statement below
--- select d.name as Department, e.name as Employee, e.salary as Salary
--- from Employee e join department d 
--- on e.departmentId=d.id
--- group by e.name
--- order by e.salary desc;
+-- select d.name as Department, e.name as Employee, salary as Salary
+-- from department d join employee e on d.id = e.departmentId
+-- where 3 > (
+--     select count(distinct e2.salary)
+--     from employee e2
+--     where e2.departmentId = d.id and e2.salary > e.salary
+-- )
 
-select d.name as Department, e.name as Employee, salary as Salary
-from department d join employee e on d.id = e.departmentId
-where 3 > (
-    select count(distinct e2.salary)
-    from employee e2
-    where e2.departmentId = d.id and e2.salary > e.salary
-)
+-- ****************************************************************************
+-- by using window function
+SELECT Department, Employee, Salary
+FROM (
+    SELECT 
+        d.name AS Department,
+        e.name AS Employee,
+        e.salary AS Salary,
+        DENSE_RANK() OVER (PARTITION BY d.name ORDER BY Salary DESC) AS rnk
+    FROM Employee e
+    JOIN Department d
+    ON e.departmentId = d.id
+) AS rnk_tbl
+WHERE rnk <= 3;
 
 
