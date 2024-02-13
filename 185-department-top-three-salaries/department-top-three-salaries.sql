bd2581f9c64c966a3346cf7e1b69d006ecdca663
Shridhar Bhardwaj <57531390+shridharbhardwaj@users.noreply.github.com>
2024-02-13 02:19:09 -0500

    Time: 2238 ms (23.07%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -7,7 +7,7 @@
 
 select d.name as Department, e.name as Employee, salary as Salary
 from department d join employee e on d.id = e.departmentId
-where 3> (
+where 3 > (
     select count(distinct e2.salary)
     from employee e2
     where e2.departmentId = d.id and e2.salary > e.salary
