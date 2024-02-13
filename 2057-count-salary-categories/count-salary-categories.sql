b4c08bbc442e16e42fadfa5c8e1c50928acf4ba0
Shridhar Bhardwaj <57531390+shridharbhardwaj@users.noreply.github.com>
2024-02-12 23:41:29 -0500

    Time: 2391 ms (72.78%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -0,0 +1,32 @@
+# Write your MySQL query statement below
+-- select case 
+-- when income < 20000 then "Low Salary"
+-- when income >= 20000 and income <= 50000 then "Average Salary"
+-- when income > 50000 then "High Salary"
+-- else 0
+-- end as category, coalesce(count(account_id),0) as accounts_count
+-- from accounts
+-- group by category;
+
+-- **************************************************************************
+SELECT 
+    'Low Salary' AS category,
+    SUM(income < 20000) AS accounts_count
+FROM 
+    Accounts
+
+UNION 
+
+    SELECT 
+        'Average Salary' AS category,
+        SUM(income BETWEEN 20000 AND 50000 ) AS accounts_count
+    FROM 
+        Accounts
+
+UNION
+
+    SELECT 
+        'High Salary' AS category,
+        SUM(income > 50000) AS accounts_count
+    FROM 
+        Accounts;
