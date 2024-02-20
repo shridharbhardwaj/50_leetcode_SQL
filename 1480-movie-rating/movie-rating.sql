013aa38dbdef15b1ea55b59b29dc7e9feee792ca
Shridhar Bhardwaj <57531390+shridharbhardwaj@users.noreply.github.com>
2024-02-20 00:12:31 -0500

    Time: 1896 ms (98.53%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -1,22 +1,21 @@
-SELECT user_name AS results
-FROM (
-SELECT u.name AS user_name, COUNT(DISTINCT mr.movie_id) AS rated_movies
-FROM Users u
-JOIN MovieRating mr ON u.user_id = mr.user_id
-GROUP BY u.user_id
-ORDER BY rated_movies DESC, user_name
-LIMIT 1
-) user_ratings
-
-UNION ALL
-
-SELECT movie_title AS results
-FROM (
-SELECT m.title AS movie_title, AVG(mr.rating) AS avg_rating
-FROM Movies m
-JOIN MovieRating mr ON m.movie_id = mr.movie_id
-WHERE DATE_FORMAT(mr.created_at, '%Y-%m') = '2020-02'
-GROUP BY m.movie_id
-ORDER BY avg_rating DESC, movie_title
-LIMIT 1
-) movie_ratings;
+(
+  select name as results
+  from movierating mr inner join users u 
+  on mr.user_id=u.user_id
+  group by u.user_id 
+  order by count(rating) desc, name
+  limit 1
+)
+union all
+(
+  select results from
+  (
+  select title as results, avg(rating) as average_rating 
+  from movierating mr 
+  inner join movies m 
+  on mr.movie_id=m.movie_id 
+  where month(created_at) = 2 and year(created_at) = '2020'
+  group by m.movie_id
+  ) rating_group
+  order by average_rating desc, results asc limit 1
+);
