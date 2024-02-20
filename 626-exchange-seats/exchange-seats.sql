e833b481af5bba2b4b15c821d46b260cfe6d4992
Shridhar Bhardwaj <57531390+shridharbhardwaj@users.noreply.github.com>
2024-02-20 00:55:57 -0500

    Time: 627 ms (65.23%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,2 +1,8 @@
-select id, coalesce(case when mod(id,2)=0 then lag(student) over(order by id) else lead(student) over(order by id) end, student) as student
-from seat;
+# Write your MySQL query statement below
+SELECT
+    CASE
+        WHEN id=(select max(id) from seat) and mod(id,2) =1 THEN id
+        WHEN MOD(id,2)=1 THEN id+1 ELSE id-1
+    END AS id, student
+FROM seat
+ORDER BY id 
